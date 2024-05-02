3636622f713313ef24971dd68ce24b0642e0d828
jtrebilcock1 <jjoetrebil@gmail.com>
2024-05-02 14:56:45 -0400

    split out html,js,css added town geojson

---
+++ map.js
@@ -0,0 +1,26 @@
+
+const map = new maplibregl.Map({
+    container: 'map',
+    style:
+        'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
+    center: [-71.7665709, 42.0532775],
+    zoom: 8
+});
+
+map.on('load', () => {
+    map.addSource('ma_towns', {
+        'type': 'geojson',
+        'data': 'geojsons/CENSUS2020TOWN.geojson'
+    });
+    map.addLayer({
+        'id': 'ma_towns',
+        'type': 'fill',
+        'source': 'ma_towns',
+        'layout': {},
+        'paint': {
+            'fill-color': 'transparent',
+            'fill-opacity': 1,
+            "fill-outline-color": "#000000"
+        }
+    });
+});
