713bfebb29f50dd61ef653cf9d2be4a2c3c81df3
jtrebilcock1 <jjoetrebil@gmail.com>
2024-05-03 14:12:17 -0400

    added popup of population changes

--- map.js
+++ map.js
@@ -1,8 +1,27 @@
+const osm_style = {
+    version: 8,
+    sources: {
+      osm: {
+        type: "raster",
+        tiles: ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
+        tileSize: 256,
+        attribution: "&copy; OpenStreetMap Contributors",
+        maxzoom: 25,
+      },
+    },
+    layers: [
+      {
+        id: "osm",
+        type: "raster",
+        source: "osm",
+      },
+    ],
+  };
+
 //create map object
 const map = new maplibregl.Map({
     container: 'map',
-    style:
-        'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
+    style: osm_style,
     center: [-71.7665709, 42.0532775],
     zoom: 8
 });
@@ -25,7 +44,36 @@ map.on('load', () => {
         'paint': {
             'fill-color': 'transparent',
             'fill-opacity': 1,
-            "fill-outline-color": "#000000"
+            "fill-outline-color": "#000000",
         }
     });
+
+        // When a click event occurs on a feature in the places layer, open a popup at the
+        // location of the feature, with description HTML from its properties.
+        map.on('click', 'ma_towns', (e) => {
+            const town_name = e.features[0].properties.TOWN20;
+            const population20 = e.features[0].properties.POP2020;
+            const population10 = e.features[0].properties.POP2010;
+            const pop_diff = population20-population10;
+            new maplibregl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(
+                    `
+                    <div class="card text-center">
+                      <div class="card-body">
+                        <p class="card-text"><strong>${town_name}</strong></p>
+                        <p class="card-text">population in 2020: <strong>${population20}</strong></p>
+                        <p class="card-text">population in 2010: <strong>${population10}</strong></p>
+                        <p class="card-text">10 year population growth: <strong>${pop_diff}</strong></p>
+                      </div>
+                    </div>
+                    `
+                )
+                .addTo(map);
+        });
+
+
+
+
+
 });
